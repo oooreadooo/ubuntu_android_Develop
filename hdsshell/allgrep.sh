77f13895605ea89aa71bfeec1517d9f16448001c
hds <289853174@qq.com>
2019-11-29 11:04:10 +0800

    完善

--- hdsshell/allgrep.sh
+++ hdsshell/allgrep.sh
@@ -14,11 +14,21 @@ function jgrep()
 {
     #kt 是kotlin
     
-    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f \( -name "*\.java" -o -name '*.kt' \) \
+    find . -name .repo -prune -o  -name .git -prune -o -name out -prune -o -type f \( -name "*\.java" -o -name '*.kt' -o -name '*.aidl' \) \
         -exec grep --color=always -nH "$@" {} +
     # find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -name "*\.java" \
     #     -exec grep --color=always -n "$@" {} +
-    echo  "支持查询的类型为:java kt"
+    echo  "支持查询的类型为:java kt aidl"
+}
+function jgrepw()
+{
+    #kt 是kotlin
+    
+    find . -name .repo -prune -o    -name .git -prune -o -name out -prune -o -type f \( -name "*\.java" -o -name '*.kt' -o -name '*.aidl' \) \
+        -exec grep --color=always -nHw "$@" {} +
+    # find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -name "*\.java" \
+    #     -exec grep --color=always -n "$@" {} +
+    echo  "支持查询的类型为:java kt aidl"
 }
 
 function mangrep()
@@ -33,6 +43,13 @@ function mgrep()
     
     find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f \( -name '*.mk' -o -name '*.mak' -o -name '*kefile' -o -name '*.make' -o -name '*.sh' -o -name '*.bp'  -o -name '*.go' \)  \
         -exec grep --color=always -nH "$@" {} +
+    echo "支持查询的类型为: mk mak kefile make bp go sh"
+}
+function mgrepw()
+{
+    
+    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f \( -name '*.mk' -o -name '*.mak' -o -name '*kefile' -o -name '*.make' -o -name '*.sh' -o -name '*.bp'  -o -name '*.go' \)  \
+        -exec grep --color=always -nHw "$@" {} +
     echo "支持查询的类型为: mk mak kefile make bp go sh"
 }
 
@@ -52,11 +69,33 @@ function xgrep()
 
         # find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -name "*\.xml"  -exec grep --color=always -n "$@" {} +
 }
+function cgrepw()
+{
+    
+    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f \( -name '*.c' -o -name '*.cc' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+        -exec grep --color=always -nHw "$@" {} +
+        echo "支持查询的类型为: c cc cpp h hpp"
+}
+function xgrepw()
+{
+    
+    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -name "*\.xml" \
+        -exec grep --color=always -nHw "$@" {} +
+    echo "支持查询的类型为: xml"
+
+        # find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -name "*\.xml"  -exec grep --color=always -n "$@" {} +
+}
 #该函数是根据给的后缀名来搜索文件的
 function hdsgrep()
 {
     find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -name "*\.$1" \
         -exec grep --color=always -nH "$2" {} +
+    
+}
+function hdsgrepw()
+{
+    find . -name .repo -prune -o -name .git -prune -o -name out -prune -o -type f -name "*\.$1" \
+        -exec grep --color=always -nHw "$2" {} +
     
 }
 
@@ -71,3 +110,12 @@ grepp_function $@
 #    $@：表示所有的参数
 
 #grep 的参数-H表示就算只有一个文件也显示文件名,没加-H之前查询单个文件不显示文件名
+
+# find的参数:
+# -not            #非，取反
+# -user           #文件所有人
+# -group          #文件所有组
+# -a              #并且关系
+# -o              #或者关系
+# -prune          #排除该文件夹
+
